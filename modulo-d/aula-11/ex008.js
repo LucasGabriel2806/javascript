b9400f6a8d9398dacf6f70840b4ee644543fb5a7
Lucas G. de Paula Pinto <102253585+LucasGabriel2806@users.noreply.github.com>
2024-11-24 19:51:35 -0300

    ex008, ex009, ex010 e ex011

---
+++ modulo-d/aula-11/ex008.js
@@ -0,0 +1,11 @@
+/** Para usar a extensão Node.js Exec aperte f8 */
+// console.log('O console funcionou corretamente');
+
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { // Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+
+console.log('Dirija sempre usando o cinto de segurança');
